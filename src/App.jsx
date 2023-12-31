468b1dba5843f20951a24418b3d2f626131ffc1c
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-01 00:34:33 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
+import { useDispatch } from 'react-redux';
+import { getApiConfiguration } from './services/store/homeSlice';
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
   return <div className='App'>App</div>;
 }
 
