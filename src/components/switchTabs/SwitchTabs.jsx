3d44c1a87570412a3a6ae5bf109b37a0b2f5c88a
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-02 13:10:37 +0530

    switch tabs

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
 import './style.scss';
-const SwitchTabs = () => {
-  return <div>SwitchTabs</div>;
+const SwitchTabs = ({ data, onTabChange }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const activeTab = (tab, index) => {
+    setLeft(index * 100);
+    setTimeout(() => {
+      setSelectedTab(index);
+    }, 300);
+    onTabChange(tab, index);
+  };
+
+  return (
+    <div className='switchingTabs'>
+      <div className='tabItems'>
+        {data?.map((tab, index) => (
+          <span
+            key={tab}
+            className={`tabItem ${selectedTab === index ? 'active' : ''}`}
+            onClick={() => activeTab(tab, index)}>
+            {tab}
+          </span>
+        ))}
+        <span
+          className='movingBg'
+          style={{ left }}
+        />
+      </div>
+    </div>
+  );
 };
 export default SwitchTabs;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import useFetch from '../../../utils/hooks/useFetch';
 
 const Trending = () => {
+  const [endpoint, setEndpoint] = useState('day');
+
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+
+  const onTabChange = (tab) => {
+    setEndpoint(tab === 'Day' ? 'day' : 'week');
+  };
+
   return (
     <div className='carouselSection'>
       <ContentWrapper>
         <span className='carouselTitle'>Trending</span>
-        <SwitchTabs data={['Day', 'Week']} />
+        <SwitchTabs
+          data={['Day', 'Week']}
+          onTabChange={onTabChange}
+        />
       </ContentWrapper>
     </div>
   );
