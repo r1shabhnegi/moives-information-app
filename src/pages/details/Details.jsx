abbcf9d4672d5d227cbb05a610761e6666919fc1
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-04 23:37:18 +0530

    details - credits and video icon

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,20 @@
 import DetailsBanner from './detailsBanner/DetailsBanner';
 import './style.scss';
+import { useParams } from 'react-router-dom';
+import useFetch from '../../utils/hooks/useFetch';
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner
+        video={data?.results[0]}
+        crew={credits?.crew}
+      />
     </div>
   );
 };
